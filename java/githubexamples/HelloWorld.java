0d8cae4704a8d7b52d77a7e14c19d432ae545e0a
ravi <rrk736@gmail.com>
2018-02-23 18:46:59 +0530

    public

--- java/githubexamples/HelloWorld.java
+++ java/githubexamples/HelloWorld.java
@@ -1,6 +1,6 @@
 package com.java.githubexamples;
 
-puvlic class HelloWorld {
+public class HelloWorld {
 	
 	public static void main(String args[]){
 		System.out.println("Hello World for Jenkins......");
